08de772cc318300998bb6620423bb164870f6666
Alan Fernandez <alan.f@msn.com>
2024-09-28 20:39:01 +0200

    orbit controls, stats, camera and lights adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stats } from '@react-three/drei';
 import RotatingCube from './RotatingCube';
 
 const App: React.FC = () => {
   return (
-    <Canvas>
+    <Canvas
+      camera={{ position: [5, 5, 5], fov: 75 }}
+      style={{
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
       <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
+      <directionalLight
+        color="red"
+        position={[10, 10, 5]}
+        intensity={1.5}
+        castShadow={true}
+      />
+
+      <Stats />
+
+      <OrbitControls />
+
       <RotatingCube />
     </Canvas>
   );
