a855a8eb34881cc2c3368cfd93bc1b3b6509cd47
Alan Fernandez <alan.f@msn.com>
2024-09-28 20:46:34 +0200

    added leva controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stats } from '@react-three/drei';
+import { Leva, useControls } from 'leva';
 import RotatingCube from './RotatingCube';
 
 const App: React.FC = () => {
+  const {
+    lightIntensity,
+    cubeColor,
+    cubePositionX,
+    cubePositionY,
+    cubePositionZ,
+  } = useControls({
+    lightIntensity: { value: 1.5, min: 0, max: 5, step: 0.1 },
+    cubeColor: '#ff6347',
+    cubePositionX: { value: 0, min: -10, max: 10 },
+    cubePositionY: { value: 0, min: -10, max: 10 },
+    cubePositionZ: { value: 0, min: -10, max: 10 },
+  });
+
   return (
-    <Canvas
-      camera={{ position: [5, 5, 5], fov: 75 }}
-      style={{
-        width: '100vw',
-        height: '100vh',
-      }}
-    >
-      <ambientLight intensity={0.1} />
-      <directionalLight
-        color="red"
-        position={[10, 10, 5]}
-        intensity={1.5}
-        castShadow={true}
-      />
+    <>
+      <Canvas
+        camera={{ position: [5, 5, 5], fov: 75 }}
+        style={{
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <ambientLight intensity={0.1} />
+        <directionalLight
+          color="red"
+          position={[10, 10, 5]}
+          intensity={lightIntensity}
+          castShadow={true}
+        />
 
-      <Stats />
+        <Stats />
 
-      <OrbitControls />
+        <OrbitControls />
 
-      <RotatingCube />
-    </Canvas>
+        <RotatingCube
+          position={[cubePositionX, cubePositionY, cubePositionZ]}
+          color={cubeColor}
+        />
+      </Canvas>
+
+      <Leva />
+    </>
   );
 };
 
--- src/RotatingCube.tsx
+++ src/RotatingCube.tsx
@@ -4,10 +4,12 @@ import * as THREE from 'three';
 
 interface RotatingCubeProps {
   position?: [number, number, number];
+  color?: string;
 }
 
 const RotatingCube: React.FC<RotatingCubeProps> = ({
   position = [0, 0, 0],
+  color = 'red',
 }) => {
   const cubeRef = useRef<THREE.Mesh>(null);
 
@@ -19,9 +21,9 @@ const RotatingCube: React.FC<RotatingCubeProps> = ({
   });
 
   return (
-    <mesh ref={cubeRef} position={position}>
+    <mesh ref={cubeRef} position={position} castShadow receiveShadow>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
